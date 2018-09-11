bae18c8ecd45a3c3227f1ffd01f5231dc7f22eb5
myflying <512710257@qq.com>
2018-09-11 14:31:38 +0800

    add test UI

---
+++ pages/testplay/subjects.js
@@ -0,0 +1,64 @@
+module.exports = [{
+    id: 'msg1',
+    snum: 1,
+    messageType: 2,
+    url: '../../assets/images/logo.png',
+    result_path:'',
+    options: [{
+        sub_title: '通过测试发现真实的自己，看看什么是自己的软肋与天赋',
+      }, {
+        sub_title: '请点击开始测试',
+      }
+    ]
+  }, {
+    id: 'msg2',
+    snum: 2,
+    sub_title: '下面有两面镜子',
+    sub_title1: '你更喜欢哪个？',
+    messageType: 1,
+    url: '../../assets/images/logo.png',
+    options: [{
+      img: '../../assets/images/sub1_options1.png',
+      options_item: 1,
+      btn_name: '日出'
+    }, {
+      img: '../../assets/images/sub1_options2.png',
+      options_item: 2,
+      btn_name: '日落'
+    }]
+  },
+  {
+    id: 'msg3',
+    snum: 3,
+    sub_title: '城堡的墙上挂着一幅画',
+    sub_title1: '你认为画中的谁是善良的？',
+    url: '../../assets/images/logo.png',
+    messageType: 1,
+    options: [{
+      img: '../../assets/images/girl.png',
+      options_item: 3,
+      btn_name: '女孩'
+    }, {
+      img: '../../assets/images/wolf.png',
+      options_item: 4,
+      btn_name: '狼'
+    }]
+  },
+  {
+    id: 'msg4',
+    snum: 4,
+    sub_title: '城堡的主人送了你一只百灵鸟',
+    sub_title1: '它能唱出世界上最动听的歌声你决定让它怎样唱歌？',
+    url: '../../assets/images/logo.png',
+    messageType: 1, 
+    options: [{
+      img: '../../assets/images/free.png',
+      options_item: 5,
+      btn_name: '自由的歌唱'
+    }, {
+      img: '../../assets/images/for_me.png',
+      options_item: 6,
+      btn_name: ' 唱你爱听的'
+    }]
+  }
+]
---
+++ pages/testplay2/subjects.js
@@ -0,0 +1,36 @@
+module.exports = [{
+    id: 'msg1',
+    snum: 1,
+    messageType: 2,
+    url: '../../assets/images/logo.png',
+    result_path:'',
+    options: [
+      {
+        sub_type: 0,
+        sub_title: '通过测试发现真实的自己，看看什么是自己的软肋与天赋',
+      }, 
+      {
+        sub_type: 1,
+        sub_title: '../../assets/images/temp1.png',
+      },{
+        sub_type: 0,
+        sub_title: '请点击开始测试',
+      }
+    ]
+  }, {
+    id: 'msg2',
+    snum: 2,
+    sub_title: '告诉我你的性别',
+    messageType: 1,
+    url: '../../assets/images/logo.png',
+    options: []
+  },
+  {
+    id: 'msg3',
+    snum: 3,
+    sub_title: '告诉我你的名字',
+    url: '../../assets/images/logo.png',
+    messageType: 1,
+    options: []
+  }
+]
